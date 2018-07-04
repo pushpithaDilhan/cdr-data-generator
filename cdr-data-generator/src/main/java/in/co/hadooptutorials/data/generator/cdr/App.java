b2be4a71cf65103dcbe33e361f940dd766e72127
PushpithaDilhan <pushpitha.14@cse.mrt.ac.lk>
2018-07-05 00:36:29 +0530

    added cdr id UUID

--- cdr-data-generator/src/main/java/in/co/hadooptutorials/data/generator/cdr/App.java
+++ cdr-data-generator/src/main/java/in/co/hadooptutorials/data/generator/cdr/App.java
@@ -3,6 +3,7 @@ package in.co.hadooptutorials.data.generator.cdr;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.UUID;
 
 import org.fluttercode.datafactory.impl.DataFactory;
 import org.joda.time.DateTime;
@@ -20,6 +21,9 @@ public class App {
         ArrayList<String> providers = new ArrayList<String>(Arrays.asList("MT","HT","ET","BA"));
         for (int i = 0; i < 1000; i++) {
 
+            // cdr ID
+            UUID id = UUID.randomUUID();
+
 			// calling party
 			String calling_num = df.getNumberText(10);
 			String calling_tower_id = "DA"+ r.nextInt(10000);
@@ -35,7 +39,7 @@ public class App {
 		    // duration in seconds
             int duration = r.nextInt(200);
 
-			System.out.println(calling_num+"|"+calling_tower_id+"|"+called_num+"|"+recipient_tower_id+"|"+d1.toString()+"|"+duration);
+			System.out.println(id.toString()+"|"+calling_num+"|"+calling_tower_id+"|"+called_num+"|"+recipient_tower_id+"|"+d1.toString()+"|"+duration);
 		}
 	}
 }
